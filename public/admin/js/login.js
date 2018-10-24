dc325e339f9c3e4e3ec6054c46aadb46c3c84fba
tanghuiqiong <1311559312@qq.com>
2018-10-24 16:42:37 +0800

    登录功能完成了

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -11,6 +11,9 @@ $(function(){
             max:6,
             message:'用户名长度必须是2-6位'
           },
+          callback:{
+            message:'用户名不存在'
+          }
         }
       },
       password:{
@@ -23,9 +26,49 @@ $(function(){
             max:12,
             message:'用户密码的长度必须是6-12位'
           },
+          callback:{
+            message:'密码错误'
+          }
         }
       },
       
-    }
+    },
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
   });
+  $('form').on('success.form.bv',function(e){
+    e.preventDefault();
+    $.ajax({
+      url:"/employee/employeeLogin",
+      type:'post',
+      data:$('form').serialize(),
+      success:function(info){
+        // console.log(info);
+        if(info.success){
+          location.href = 'index.html'
+        }
+        if(info.error === 1000){
+          // alert('用户名不存在');
+          $('form')
+            .data('bootstrapValidator')
+            .updateStatus('username','INVALID','callback')
+        } 
+        if(info.error === 1001){
+         $('form')
+            .data('bootstrapValidator')
+            .updateStatus('password','INVALID','callback')
+        }
+      }
+    })
+  })
+
+  //重置表单功能
+  $('[type=reset]').on('click',function(){
+    $('form')
+        .data('bootstrapValidator')
+        .resetForm(true)
+  })
 })
